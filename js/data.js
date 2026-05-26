650ab2b770f58314ef6990a418d78aafebf1bffd
rickulinio <rickulinio@gmail.com>
2026-05-27 00:00:28 +0200

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,29 +1,46 @@
 const FACTIONS = [
-
   {
     key: 'adm', 
     name: 'ADMINISTRACJA',
     icon: '🚨',
     color: '#ff0000',
     desc: "Dołącz do grona administracji Vast Roleplay.",
-
-    webhook: 'https://discord.com/api/webhooks/1506379285898985635/5g2imypeguUg_2eXyDrdyCLJuRAYDghkY9Ak5NCr7GSHm85mhcWXyf2Y82ywUvbbuJbi',
-    roleId: '',
-
+    webhook: 'https://discord.com/api/webhooks/...',
+    roleId: '123456789',
     questions: [
       {
         section: 'OOC',
         items: [
           {
             id: 'adm1',
-            label: 'Nazwa + Discord ID',
-            type: 'input',
-            required: true
+            label: 'Dlaczego chcesz dołączyć?',
+            type: 'textarea',
+            required: true,
+            maxLength: 800 // Limit 800 znaków
           },
+          {
+            id: 'adm2',
+            label: 'Ile masz lat?',
+            type: 'input',
+            required: true,
+            maxLength: 3
+          }
         ]
       },
+      {
+        section: 'Doświadczenie',
+        items: [
+          {
+            id: 'adm3',
+            label: 'Gdzie byłeś w administracji?',
+            type: 'textarea',
+            required: true,
+            maxLength: 1200
+          }
+        ]
+      }
     ]
-  },
+  }
 ];
 
 const TEAM = [
